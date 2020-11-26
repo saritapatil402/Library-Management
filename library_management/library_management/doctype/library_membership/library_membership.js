7ad2f7921e055dedb88161b47f691dcda5062acb
saritapatil402 <saritapatil402@gmail.com>
2020-11-26 10:06:49 +0530

    first commit

---
+++ library_management/library_management/doctype/library_membership/library_membership.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, sarita and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Library Membership', {
+	// refresh: function(frm) {
+
+	// }
+});
